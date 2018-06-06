b7dcc64b91ab33173b5a26442bb97650a56e1a81
Amanda Rawls <jennifer.amanda.rawls@monsanto.com>
2018-06-06 06:37:15 -0500

    print documentDirectory url

--- GardenGuardian/Core Data/Persistence.swift
+++ GardenGuardian/Core Data/Persistence.swift
@@ -11,7 +11,7 @@ class Persistence {
         }
         
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+    
         return managedContext
     }()
     
--- GardenGuardian/ViewController.swift
+++ GardenGuardian/ViewController.swift
@@ -14,10 +14,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let flower = Flower(name: "Coral Bells 'Palace Purple'", height: 20, width: 12, spacing: 12, hardiness: -30, water: 1)
-        let cdFlower = persistenceLayer.save(flower: flower)
-        print("flower: \(flower)")
-        print("cdFlower: \(cdFlower)")
+//        let flower = Flower(name: "Coral Bells 'Palace Purple'", height: 20, width: 12, spacing: 12, hardiness: -30, water: 1)
+//        let cdFlower = persistenceLayer.save(flower: flower)
+//        print("flower: \(flower)")
+//        print("cdFlower: \(cdFlower)")
+        
+        guard let fetchedFlower = persistenceLayer.flower(with: "Coral Bells 'Palace Purple'") else {
+            print("unable to retrieve flower")
+            return
+        }
+        print(fetchedFlower)
         
     }
 
